378ebcbe9777ac8c11f341797778c57e883d968a
girikum <girikum@umich.edu>
2018-01-27 18:52:02 -0500

    Replace control_station.py

--- armlab-W18/control_station.py
+++ armlab-W18/control_station.py
@@ -2,6 +2,7 @@
 import sys
 import cv2
 import numpy as np
+from kinematics import *
 from PyQt4 import QtGui, QtCore, Qt
 from ui import Ui_MainWindow
 from rexarm import Rexarm
@@ -62,8 +63,17 @@ class Gui(QtGui.QMainWindow):
         Connect Sliders to Function
         TODO: CONNECT THE OTHER 5 SLIDERS IMPLEMENTED IN THE GUI 
         """ 
-        self.ui.sldrBase.valueChanged.connect(self.sliderChange)
-
+        self.ui.sldrBase.valueChanged.connect(self.sliderBaseChange)
+        self.ui.sldrShoulder.valueChanged.connect(self.sliderShoulderChange)     #Giri
+        self.ui.sldrElbow.valueChanged.connect(self.sliderElbowChange) 	 #Giri
+        self.ui.sldrWrist.valueChanged.connect(self.sliderWristChange)        #Giri
+	self.ui.sldrGrip1.valueChanged.connect(self.sliderGrip1Change)        #Giri
+        self.ui.sldrGrip2.valueChanged.connect(self.sliderGrip2Change)        #Giri        
+	self.ui.sldrMaxTorque.valueChanged.connect(self.sliderOtherChange)        #Giri
+        self.ui.sldrSpeed.valueChanged.connect(self.sliderOtherChange)        #Giri        
+	self.ui.sldrGrip1.setEnabled(True) #Giri
+	self.ui.sldrGrip2.setEnabled(True) #Giri
+	
         """ Initial command of the arm to home position"""
         self.sliderChange() 
         
@@ -72,8 +82,6 @@ class Gui(QtGui.QMainWindow):
         """
         self.ui.btnUser1.setText("Configure Servos")
         self.ui.btnUser1.clicked.connect(self.rex.cfg_publish_default)
-#        self.ui.btnUser2.setText("Simple Calibration") 
-#        self.ui.btnUser2.clicked.connect(self.simple_cal)
         self.ui.btnUser12.setText("Emergency Stop!")
         self.ui.btnUser12.clicked.connect(self.estop)
 
@@ -84,11 +92,41 @@ class Gui(QtGui.QMainWindow):
 	self.ui.btnUser3.clicked.connect(self.extrinsic_cali)
 	self.ui.btnUser4.setText("Block Detector") #Josh
 	self.ui.btnUser4.clicked.connect(self.video.blockDetector)
-	
+	self.ui.btnUser5.setText("Pick Block")
+	self.ui.btnUser5.clicked.connect(self.pick)
+	self.ui.btnUser6.setText("Place Block")
+	self.ui.btnUser6.clicked.connect(self.place)
 
+	self.ui.btnUser7.setText("Draw")
+	self.ui.btnUser7.clicked.connect(self.teach) #Giri
+	self.ui.btnUser8.setText("Enter Competition")
+	self.ui.btnUser8.clicked.connect(self.teach) #Giri
+
+
+		
+	self.ui.btnUser9.setText("Teach")
+	self.ui.btnUser9.clicked.connect(self.teach) #Giri
+        self.ui.btnUser10.setText("Repeat")
+	self.ui.btnUser10.clicked.connect(self.repeat) #Giri
+	self.ui.btnUser11.setText("Smooth Repeat")
+	self.ui.btnUser11.clicked.connect(self.srepeat) #Giri        
+	self.ui.btnUser11.setEnabled(False) #Giri			
+	
 
     def estop(self):
         self.statemachine.estop(self.rex)
+
+    def trim_angle(self,q):
+	if(q>360 or q<-360):
+		new_q = 0
+	elif(q>180): #181 --> -179
+		new_q = q-360
+	elif(q<-180):
+		new_q = 360-q
+	else :
+		new_q = q
+	return new_q
+
 
     def update_gui(self):
         """ 
@@ -125,7 +163,25 @@ class Gui(QtGui.QMainWindow):
         self.ui.rdoutShoulderJC.setText(str("%.2f" % (self.rex.joint_angles_fb[1]*R2D)))
         self.ui.rdoutElbowJC.setText(str("%.2f" % (self.rex.joint_angles_fb[2]*R2D)))
         self.ui.rdoutWristJC.setText(str("%.2f" % (self.rex.joint_angles_fb[3]*R2D)))
+	self.ui.rdoutGrip1.setText(str(self.ui.sldrGrip1.value() )) #Giri
+        self.ui.rdoutGrip2.setText(str(self.ui.sldrGrip2.value() )) #Giri
+        
 
+	"""
+        Update End Effector Coordinates Kevin
+	"""        
+	endCoord=forwardKinematics(self.rex.joint_angles_fb[0]*R2D,self.rex.joint_angles_fb[1]*R2D,self.rex.joint_angles_fb[2]*R2D,self.rex.joint_angles_fb[3]*R2D)
+        self.ui.rdoutX.setText(str("%2f" %(round(endCoord[0],2)) ) )
+        self.ui.rdoutY.setText(str("%2f" %(round(endCoord[1],2)) ) )
+        self.ui.rdoutZ.setText(str("%2f" %(round(endCoord[2],2)) ) )
+        self.ui.rdoutT.setText(str("%2f" %(round(endCoord[3],2)) ) )
+	
+	angles = inverseKinematics(endCoord[0],endCoord[1],endCoord[2],endCoord[3])
+	angles[0] = round(self.trim_angle(angles[0]),2)
+	angles[1] = round(self.trim_angle(angles[1]),2)	
+	angles[2] = round(self.trim_angle(angles[2]),2)
+	angles[3] = round(self.trim_angle(angles[3]),2)
+	#print angles
         """ 
         Mouse position presentation in GUI
         TODO: after implementing workspace calibration 
@@ -189,12 +245,82 @@ class Gui(QtGui.QMainWindow):
         self.ui.rdoutBase.setText(str(self.ui.sldrBase.value()))
         self.ui.rdoutTorq.setText(str(self.ui.sldrMaxTorque.value()) + "%")
         self.rex.torque_multiplier = self.ui.sldrMaxTorque.value()/100.0
-        self.rex.speed_multiplier = self.ui.sldrSpeed.value()/100.0
+        self.rex.speed_multiplier = [self.ui.sldrSpeed.value()/100.0]*self.rex.num_joints
         self.rex.joint_angles[0] = self.ui.sldrBase.value()*D2R
         self.rex.joint_angles[1] = self.ui.sldrShoulder.value()*D2R
         self.rex.joint_angles[2] = self.ui.sldrElbow.value()*D2R
         self.rex.joint_angles[3] = self.ui.sldrWrist.value()*D2R
         self.rex.cmd_publish()
+
+    def sliderinitChange(self): #Giri  
+        """ 
+        Function to change the slider labels when sliders are moved
+        and to command the arm to the given position
+        """
+        self.ui.rdoutBase.setText(str(self.ui.sldrBase.value()))
+	self.ui.rdoutShoulder.setText(str(self.ui.sldrShoulder.value() )) #Giri
+        self.ui.rdoutElbow.setText(str(self.ui.sldrElbow.value() )) #Giri
+        self.ui.rdoutWrist.setText(str(self.ui.sldrWrist.value() )) #Giri
+        self.ui.rdoutGrip1.setText(str(self.ui.sldrGrip1.value() )) #Giri
+        self.ui.rdoutGrip2.setText(str(self.ui.sldrGrip2.value() )) #Giri
+        
+        self.ui.rdoutTorq.setText(str(self.ui.sldrMaxTorque.value()) + "%")
+	self.ui.rdoutSpeed.setText(str(self.ui.sldrSpeed.value()) + "%")	        
+        
+	self.rex.torque_multiplier = self.ui.sldrMaxTorque.value()/100.0
+        self.rex.speed_multiplier = [self.ui.sldrSpeed.value()/100.0]*self.rex.num_joints
+        #Giri{
+	self.rex.joint_angles[0] = self.ui.sldrBase.value()*D2R  
+        self.rex.joint_angles[1] = self.ui.sldrShoulder.value()*D2R
+        self.rex.joint_angles[2] = self.ui.sldrElbow.value()*D2R
+        self.rex.joint_angles[3] = self.ui.sldrWrist.value()*D2R
+	if(len(self.rex.joint_angles) == 6):	
+		self.rex.joint_angles[4] = self.ui.sldrGrip1.value()*D2R
+        	self.rex.joint_angles[5] = self.ui.sldrGrip2.value()*D2R
+        #Giri}        
+	self.rex.cmd_publish()
+	
+    #From Giri: I implemented all these function in a one function above, incase if the response time is bad, we can go back to the below five separate functions
+
+    def sliderOtherChange(self): #Giri
+	self.ui.rdoutTorq.setText(str(self.ui.sldrMaxTorque.value()) + "%")
+	self.ui.rdoutSpeed.setText(str(self.ui.sldrSpeed.value()) + "%")	        
+	self.rex.torque_multiplier = self.ui.sldrMaxTorque.value()/100.0
+        self.rex.speed_multiplier = [self.ui.sldrSpeed.value()/100.0]*self.rex.num_joints
+        self.rex.cmd_publish()
+	    
+    def sliderBaseChange(self): #Kevin
+        self.ui.rdoutBase.setText(str(self.ui.sldrBase.value()))
+        self.rex.joint_angles[0] = self.ui.sldrBase.value()*D2R
+        self.rex.cmd_publish()
+
+    def sliderShoulderChange(self): #Kevin
+        self.ui.rdoutShoulder.setText(str(self.ui.sldrShoulder.value() ))
+        self.rex.joint_angles[1] = self.ui.sldrShoulder.value()*D2R
+        self.rex.cmd_publish()
+
+    def sliderElbowChange(self): #Kevin
+        self.ui.rdoutElbow.setText(str(self.ui.sldrElbow.value() ))
+        self.rex.joint_angles[2]=self.ui.sldrElbow.value()*D2R
+        self.rex.cmd_publish()
+
+    def sliderWristChange(self): #Kevin
+        self.ui.rdoutWrist.setText(str(self.ui.sldrWrist.value() ))
+        self.rex.joint_angles[3]=self.ui.sldrWrist.value()*D2R
+        self.rex.cmd_publish()
+
+
+    def sliderGrip1Change(self): #Giri
+        self.ui.rdoutGrip1.setText(str(self.ui.sldrGrip1.value() ))
+        self.rex.joint_angles[4]=self.ui.sldrGrip1.value()*D2R
+        self.rex.cmd_publish()
+
+    def sliderGrip2Change(self): #Giri
+        self.ui.rdoutGrip2.setText(str(self.ui.sldrGrip2.value() ))
+        self.rex.joint_angles[5]=self.ui.sldrGrip2.value()*D2R
+        self.rex.cmd_publish()
+    
+
 
     def mousePressEvent(self, QMouseEvent):
         """ 
@@ -324,7 +450,61 @@ class Gui(QtGui.QMainWindow):
 	self.video.distortion_array = np.float32([2.45479098e-01,-8.27672136e-01, 1.05870966e-03, -3.01947277e-03,1.08683931e+00])
 	self.video.extrinsic_cal_flag = 1
 
+    def pick(self): #Josh
+	blockx, blocky, blockz, angle = self.get_color_block_world_coord('blue')
+	print [blockx, blocky, blockz, angle]
+	endCoord = [blockx/10.0, blocky/10.0, (blockz+30)/10.0, 90]
+	angles = inverseKinematics(endCoord[0],endCoord[1],endCoord[2],endCoord[3])
+	print "endcoord:"
+	print endCoord
+	angles[0] = round(self.trim_angle(angles[0]),2)
+	angles[1] = round(self.trim_angle(angles[1]),2)	
+	angles[2] = round(self.trim_angle(angles[2]),2)
+	angles[3] = round(self.trim_angle(angles[3]),2)
+	
+ 	self.rex.joint_angles[0] = angles[0]*D2R
+        self.rex.joint_angles[1] = angles[1]*D2R
+        self.rex.joint_angles[2] = angles[2]*D2R
+        self.rex.joint_angles[3] = angles[3]*D2R
+ 	self.rex.cmd_publish()
+    	
+ 
+    def place(self):
+	pass
+    
+    def get_color_block_world_coord(self,color): #Josh
+	self.video.blockDetector()
+	[blockx_rgb, blocky_rgb], angle = self.video.color_detection(color)
+	xy_in_rgb = np.float32([[[blocky_rgb,blockx_rgb]]]) #Josh
+	xy_in_rgb_homogenous = np.array([blocky_rgb,blockx_rgb,1.0])
+	xy_in_dep = np.dot(self.video.rgb2dep_aff_matrix, xy_in_rgb_homogenous) #Josh
+	x_dep = int(xy_in_dep[0]) #Josh
+	y_dep = int(xy_in_dep[1]) #Josh
+        d = self.video.currentDepthFrame[y_dep][x_dep]
+	Z = -1
+	for idx in range(len(self.video.levels_mm)):
+	    if d <=self.video.levels_upper_d[idx] and d>=self.video.levels_lower_d[idx]:
+		Z = 941 - self.video.levels_mm[idx]
+		break
+	camera_coord = Z*cv2.undistortPoints(xy_in_rgb,self.video.intrinsic,self.video.distortion_array)
+	camera_coord_homogenous = np.transpose(np.append(camera_coord,[Z,1.0]))
+	world_coord_homogenous = np.dot(self.video.extrinsic, camera_coord_homogenous)
+	return world_coord_homogenous[0], world_coord_homogenous[1], world_coord_homogenous[2], angle
 
+
+    def teach(self): #Giri
+        self.statemachine.teach(self.ui,self.rex) #Giri
+        	
+    def repeat(self): #Giri
+	if(self.ui.btnUser5.text() == "Repeat"):
+		self.statemachine.init(self.ui,self.rex,"Initialising")	
+	else:
+		self.statemachine.repeat(self.ui,self.rex, False) #Giri
+	
+	
+    def srepeat(self): #Giri
+	self.statemachine.init(self.ui,self.rex,"initialising")
+    	
 """main function"""
 def main():
     app = QtGui.QApplication(sys.argv)
