427ab3b84907c6ebc333ac3e5df3104d8b387d14
girikum <girikum@umich.edu>
2018-01-30 19:29:12 -0500

    Replace statemachine.py

--- armlab-W18/statemachine.py
+++ armlab-W18/statemachine.py
@@ -6,15 +6,22 @@ import functools
 import decimal
 import pylab
 
+
 D2R = 3.141592/180.0
 R2D = 180.0/3.141592
-current_state = "idle"
-past_state = "idle"
-goal_angles = [0.0]*4	
-picking = "True"
-mode ="normal" #competition1 or or normal
+current_motorstate = "idle" # possible states: motion or idle
+current_mode = "idle" # competition1 or competition2 or competition3 or competition4 or competition5 or testing
+current_action = "idle" #picking, placing
+current_movement = "idle" #picking, grabbing qi, grabbing q, grabbing qf, idle or placing, placing qi, placing q, placing qf  
+past_status = ""
+past_states = [""]*4
+qi = [0.0]*6
+qf = [0.0]*6
+q  = [0.0]*6
+#picking = "True"
+#mode ="normal"
 wrist_orientation = 0.0
-gripper_orientation = 180.0
+gripper_orientation = 90.0
 
 def gen_timestamp(usec=False): #Giri
 	t = datetime.datetime.now()
@@ -43,7 +50,6 @@ def trim_angle(q):
 	return new_q
 
 
-
 class Statemachine():
     timerCallback = functools.partial(gen_timestamp)
     def setorientation(self, w_orient, g_orient=gripper_orientation,gripper=False):
@@ -51,39 +57,59 @@ class Statemachine():
 	wrist_orientation = w_orient
 	if(gripper):
 		gripper_orientation = g_orient
-		
-    def setmode(self,set_mode):
-	global mode
-	if(set_mode== "normal" or set_mode=="competition"):
-		mode = set_mode
-	else: 
-		print "Invalid mode"
 
+    def setq(self,new_qi,new_q,new_qf):
+	global qi, q, qf
+	qi = new_qi
+	q  = new_q
+	qf = new_qf
+		
     def setme(self, ui, rex):
 	self.timerCallback = functools.partial(self.statemachine_check, ui = ui ,rex = rex)
-   
-    def setgoal(self,angles):
-	global goal_angles
-	goal_angles[0] = angles[0]
-	goal_angles[1] = angles[1]
-	goal_angles[2] = angles[2]
-	goal_angles[3] = angles[3]
-	
-    def idle(self,rex):
-	global current_state	
-	current_state = "idle"
-	#print "I am Idle"
-	rex.speed_multiplier = [0.1]*rex.num_joints
-        rex.cmd_publish()	
-    def estop(self, rexarm):
-        #sets the torque of all motors to 0, in case of emergency
-        rexarm.max_torque = [0.0, 0.0, 0.0, 0.0]
-        rexarm.cmd_publish()
-        
-        timerCallback = functools.partial(gen_timestamp)
 
+    def getmestatus(self,combined=False):
+	if(combined):
+		return current_mode + ", " + current_action + ", " + current_movement + ", " + current_motorstate
+	else:	
+		return [current_mode, current_action, current_movement, current_motorstate]
+   
+    def setmystatus(self,new_mode="Don't set", new_action="Don't set", new_movement="Don't set", new_motorstate="Don't set"):
+	global current_mode, current_action, current_motorstate, current_movement
+	if(new_mode != "Don't set"):
+		current_mode = new_mode
+	if(new_action != "Don't set"):
+		current_action = new_action
+	if(new_movement != "Don't set"):
+		current_movement = new_movement		
+	if(new_motorstate != "Don't set"):
+		current_motorstate = new_motorstate
+
+    def motor_idle(self,rex):
+	global current_motorstate	
+	rex.speed_multiplier = [0.1]*rex.num_joints
+	current_motorstate = "idle"
+	rex.cmd_publish()
+    
+    def movement_idle(self):
+	global current_movement	
+	current_movement = "idle"
+
+    def action_idle(self):
+	global current_action	
+	current_action = "idle"
+		
+
+    def mode_idle(self):
+	global current_mode	
+	current_mode = "idle"
+	
+    def estop(self, rex):
+        #sets the torque of all motors to 0, in case of emergency
+        rex.max_torque = [0.0]*rex.num_joints
+        rex.cmd_publish()
+        
     def checkmotors(self,rex):
-	atol = 5.000 #do a formal way of selecting this tolerance
+	atol = 5.00 #do a formal way of selecting this tolerance
 	"""
 	print rex.joint_angles[0]*R2D-rex.joint_angles_fb[0]*R2D
 	print rex.joint_angles[1]*R2D-rex.joint_angles_fb[1]*R2D
@@ -120,118 +146,192 @@ class Statemachine():
 		#print "motor 0 fails " + str(abs(rex.joint_angles[0]*R2D-rex.joint_angles_fb[0]*R2D)) + "angles are: " + str(rex.joint_angles[0]*R2D) + str(rex.joint_angles_fb[0]*R2D)
 		return False 	
 
+
     
     def statemachine_check(self, ui, rex): #running at 100 ms
-	global current_state, past_state
-	
-	if(current_state != past_state):
-		print current_state
-		past_state = current_state
-	"""
-	if(current_state == "basefirst"):
+	global current_mode, current_action, current_movement, current_motorstate, past_states
+	past_status = past_states[0]+", "+past_states[1]+", "+ past_states[2] + ", "+ past_states[3]
+	if(current_motorstate == "motion"):
 		if(self.checkmotors(rex)):
-			self.reach(ui,rex)
-	"""	
+			self.motor_idle(rex)
+	current_status = self.getmestatus(True)
 	
-	if(current_state == "holding"):
-		if(self.checkmotors(rex)):
-			self.reach(ui,rex)
+	if(current_status != past_status and current_motorstate == "idle"): 
+		print "current_status: " + current_status + "; past_status: " + past_status
+		print "current_motorstate: " + current_motorstate		
+		past_states = self.getmestatus(False)
+	
+		if(current_mode == "testing"):
+			if(current_action == "picking"):			
+				self.picking(ui,rex)		
+			elif(current_action =="placing"):
+				self.placing(ui,rex)
+			elif(current_movement == "idle"):
+				self.action_idle()		
+				self.mode_idle()
+		elif(current_mode == "Competition 1"):
+			if(current_action == "picking"):
+				if(current_movement == "idle"):
+					current_action = "placing"
+					current_movement = "placing"
+					new_q = [-q[0], q[1], q[2], q[3]]
+					self.setq(new_q,new_q,new_q)		
+					self.placing(ui,rex)
+				else:
+					self.picking(ui,rex)
 
-		
-	if(current_state == "picking" or current_state == "placing" or current_state == "shoulderlast picking" or current_state == "shoulderlast placing"):
-		if(self.checkmotors(rex)):
-			#print "reached " + current_state
-			if(current_state=="picking"):
-				self.close(rex)
-			elif(current_state=="placing"):
-				self.open(rex)
-			elif(current_state=="shoulderlast placing" or current_state=="shoulderlast picking"):
-				self.shoulderlast(rex)
-			else:
-				pass#print "stupid"
-		else:
-			pass#print "not reached yet"
-	
-	elif(current_state == "closing"):
-		if(self.checkmotors(rex)):
-			if(mode=="competition"):
-				self.hold(ui,rex,[-1*goal_angles[0],goal_angles[1],goal_angles[2],goal_angles[3]],"placing")
-			else:
-				self.idle(rex)		
-	
-	elif(current_state == "opening"):
-		if(self.checkmotors(rex)):
-				self.idle(rex)
+			elif(current_action == "placing"):
+				if(current_movement == "idle"):
+					self.action_idle()
+					self.mode_idle()
+				else:
+					self.placing(ui,rex)
+		elif(current_mode == "Competition 2"):
+			pass
+		elif(current_mode == "Competition 3"):
+			pass
+		elif(current_mode == "Competition 4"):
+			pass
+		elif(current_mode == "Competition 5"):
+			pass
 
-    def reach(self,ui,rex):
-	global current_state
-	rex.joint_angles[0] = goal_angles[0]*D2R
-	rex.joint_angles[2] = goal_angles[2]*D2R
-        rex.joint_angles[3] = goal_angles[3]*D2R	 	
-	rex.joint_angles[4] = wrist_orientation
-	if(abs(goal_angles[0])>90):
-		if(picking):
-			current_state = "shoulderlast picking"
-		else:
-			current_state = "shoulderlast placing"	
+
+    def picking(self,ui,rex):
+	global current_movement
+	if(current_movement == "picking"):
+		#print "1"
+		self.hold(ui,rex)
+	elif(current_movement == "grabbing qi"):
+		#print "2"		
+		self.reach(ui,rex)
+	elif(current_movement == "grabbing q"):
+		#print "3"		
+		self.close(rex)
+	elif(current_movement == "shoulderlast grabbing q" or current_movement == "shoulderlast keeping q"):
+		#print "4"		
+		self.shoulderlast(rex)
+	elif(current_movement == "keeping q"):
+		#print "5"
+		self.open(rex)
+	elif(current_movement == "closing"):
+		#print "6"		
+		self.movement_idle()		
 	else:
-	        rex.joint_angles[1] = goal_angles[1]*D2R  
-		if(picking):
-			current_state = "picking"
-		else:
-			current_state = "placing"
-		
-    	rex.cmd_publish()
+		pass#print "7:" + current_movement
 
-    def basefirst(self,rex):
-	global current_state
-	rex.joint_angles[0] = goal_angles[0]*D2R
-	current_state = "basefirst"
-	rex.cmd_publish()
+    def placing(self,ui,rex):
+	global current_movement
+	if(current_movement == "placing"):
+		self.hold(ui,rex)
+	elif(current_movement == "keeping qi"):
+		self.reach(ui,rex)
+	elif(current_movement == "shoulderlast keeping q"):
+		self.shoulderlast(rex)
+	elif(current_movement == "keeping q"):
+		self.open(rex)
+	elif(current_movement == "opening"):
+		current_movement = "shoulderfirst keeping qf"
+		self.shoulderfirst(rex)
+	elif(current_movement == "keeping qf"):
+		self.hold(ui,rex) 	
 
-    def shoulderlast(self,rex):
-	global current_state
-	rex.joint_angles[1] = goal_angles[1]*D2R
-	if(current_state=="shoulderlast picking"):
-		current_state = "picking"	
-	elif(current_state=="shoulderlast placing"):	
-		current_state = "placing"
-	rex.cmd_publish()
+
+    def getangles(self, color):
+	blockx, blocky, blockz, angle = self.get_color_block_world_coord(color)
+	endCoord = [(blockx)/10, (blocky)/10, (blockz+40)/10, gripper_orientation]	
+	angles = inverseKinematics(endCoord[0],endCoord[1],endCoord[2],endCoord[3])
+	angles[0] = round(self.trim_angle(angles[0]),2)
+	angles[1] = round(self.trim_angle(angles[1]),2)	
+	angles[2] = round(self.trim_angle(angles[2]),2)
+	angles[3] = round(self.trim_angle(angles[3]),2)
+	self.setq(angles,angles,angles)
 	
-   
+    def hold(self,ui,rex):
+	global current_motorstate, current_movement
+	if(rex.num_joints==6):	
+	
+		if(current_action == "picking"):
+			rex.joint_angles[5] = 90*D2R
+			current_movement = "grabbing qi"
+		else:
+			rex.joint_angles[5] = -90*D2R
+			if(current_movement == "keeping qf"):
+				self.movement_idle()
+			else:
+				current_movement = "keeping qi"
 
-    def close(self,rex):
-	global current_state
-	#print "Hello from pick"
-	rex.joint_angles[5] = -90*D2R
-	rex.cmd_publish()
-	current_state = "closing"    		
-
-    def open(self,rex):
-	global current_state
-	rex.joint_angles[5] = 90*D2R
-	rex.cmd_publish()
-	current_state = "opening"
-
-    def hold(self,ui,rex,angles,state):
-	global current_state,goal_angles, picking
-	goal_angles[0] = angles[0]
-	goal_angles[1] = angles[1]
-	goal_angles[2] = angles[2]
-	goal_angles[3] = angles[3]
-	if(state == "picking"):
-		picking = True
-		rex.joint_angles[5] = 90*D2R
-	else:
-		rex.joint_angles[5] = -90*D2R
-		picking = False
+		rex.joint_angles[4] = 0.0	
 	rex.joint_angles[0] = 0.0
         rex.joint_angles[1] = 0.0
         rex.joint_angles[2] = 0.0
         rex.joint_angles[3] = 0.0
-     	rex.joint_angles[4] = 0.0
-		
+	current_motorstate = "motion" 
 	rex.cmd_publish()
-	current_state = "holding"
+
+    def reach(self,ui,rex):
+	global current_motorstate,current_movement
+	rex.joint_angles[0] = q[0]*D2R #goal_angles[0]*D2R
+	rex.joint_angles[2] = q[2]*D2R #goal_angles[2]*D2R
+        rex.joint_angles[3] = q[3]*D2R #goal_angles[3]*D2R
+	if(rex.num_joints==6):		
+		rex.joint_angles[4] = wrist_orientation
+	if(abs(q[0])>90):
+		if(current_movement == "grabbing qi"):
+			current_movement = "shoulderlast grabbing q"
+		else:
+			current_movement = "shoulderlast keeping q"	
+	else:
+	        rex.joint_angles[1] = q[1]*D2R  
+		if(current_movement == "grabbing qi"):
+			current_movement = "grabbing q"
+		else:
+			current_movement = "keeping q"
+	current_motorstate = "motion"	
+    	rex.cmd_publish()
+
+    def basefirst(self,rex):
+	global current_motorstate,current_movement
+	rex.joint_angles[0] = q[0]*D2R
+	current_motorstate = "motion"	
+    	rex.cmd_publish()
+
+    def shoulderlast(self,rex):
+	global current_motorstate,current_movement
+	rex.joint_angles[1] = q[1]*D2R
+	if(current_movement=="shoulderlast grabbing q"):
+		current_movement = "grabbing q"	
+	elif(current_movement=="shoulderlast keeping q"):	
+		current_movement = "keeping q"
+	current_motorstate = "motion"		
+	rex.cmd_publish()
+	
+    def shoulderfirst(self,rex):
+	global current_motorstate,current_movement
+	rex.joint_angles[1] = 0.0
+	if(current_movement=="shoulderfirst grabbing qf"):
+		current_movement = "grabbing qf"	
+	elif(current_movement=="shoulderfirst keeping qf"):	
+		current_movement = "keeping qf"
+	current_motorstate = "motion"		
+	rex.cmd_publish()
+
+
+    def close(self,rex):
+	global current_motorstate, current_movement
+	#print "Hello from pick"
+	if(rex.num_joints==6):	
+		rex.joint_angles[5] = -90*D2R
+	current_movement = "closing"   
+	current_motorstate = "motion"	 		
+	rex.cmd_publish()
+    	
+    def open(self,rex):
+	global current_motorstate, current_movement
+	if(rex.num_joints==6):	
+		rex.joint_angles[5] = 90*D2R
+	current_movement = "opening"
+	current_motorstate = "motion"	
+	rex.cmd_publish()
+	
 	
 	    		
