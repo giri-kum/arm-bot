139fd74c0fbf52b870e605cc4b92f7b00c05958f
Kevin <kxia@umich.edu>
2018-02-04 23:51:51 -0500

    Update pyramid.py

--- armlab-W18/pyramid.py
+++ armlab-W18/pyramid.py
@@ -1,10 +1,12 @@
-def pyramid(nlayers,spacing) #returns numpy array.
-	coords=zeros(3,nlayers*(nlayers+1)/2)
+from numpy import *
+def pyramid(nlayers,spacing): #returns numpy array.
+	coords=zeros((3,nlayers*int((nlayers+1)/2)))
 	count=0
-	for j in 0 to nlayers-1
-		for i in 0 to nlayers-j-1
-			x[count]=0+spacing*(i-((n-j)/2))
-			y[count]=-8.95
-			z[count]=offset+(blockheight*j) #define blockheight and offset 
+	for j in range(0, nlayers):
+		for i in range(0 , nlayers-j):
+			print(nlayers-j-1)
+			coords[0][count]=0+spacing*(i-((nlayers-j)/2))
+			coords[1][count]=-8.95
+			coords[2][count]=offset+(blockheight*j) #define blockheight and offset 
 			count=count+1
 	return coords
