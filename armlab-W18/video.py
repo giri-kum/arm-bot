27cedeec6748266bbdc8393f553641c3cd1e479a
girikum <girikum@umich.edu>
2018-02-07 17:59:49 -0500

    Replace video.py final competition day

--- armlab-W18/video.py
+++ armlab-W18/video.py
@@ -65,7 +65,7 @@ class Video():
 	self.extrinsic_cal_flag = 0 #Josh
 	self.extrinsic_cal_pixel_coord = np.float32([[0.0, 0.0],[0.0, 0.0],[0.0, 0.0]]) #Josh
 	self.extrinsic_cal_world_coord = np.float32([[-305., -305., 0.],[-305., 305.,0.],[305.0, 305.0,0.]]) #Josh
- 	self.levels_mm = np.float32([0., 19.25, 57.5, 96.25, 134.75, 173.25, 211.75, 250.25])
+ 	self.levels_mm = np.float32([0., 19.25, 57.75, 96.25, 134.75, 173.25, 211.75, 250.25])
 	self.levels_lower_d = np.float32([716,705,687,668,648,624,598,570,538])#np.float32([716,697,680,659,637,615,586,557,530])
 	self.levels_upper_d = np.float32([726,710,692,675,653,630,604,575,543])#np.float32([726,715,696,679,658,636,614,585,556])
 	self.depth_image_min_row = 0
@@ -79,7 +79,7 @@ class Video():
 	self.blobs_angle_rgb = []
 	self.blobs_info_rgb = []
 	#due to light from roof, yellow shows 0,0,254 in hsv color space. red goes to two ends of hsv spectral, needs to consider seperately
-	self.hsv_lower_hsv = [[0.,9.,15.],[4.,100.,100.],[20.,90.,200.],[40.,30.,75.],[110.,50.,80.],[130.,70.,60.],[164.,150.,170.],[0.,0.,0.]] #Josh
+	self.hsv_lower_hsv = [[0.,9.,15.],[4.,100.,100.],[20.,90.,200.],[40.,30.,75.],[110.,50.,80.],[130.,70.,60.],[164.,50.,170.],[0.,0.,0.]] #Josh
 	self.hsv_upper_hsv = [[180.,243.,90.],[20.,255.,255.],[39.,255.,255.],[85.,200.,210.],[129.,255.,255.],[168.,190.,160.],[173.,210.,255.],[180.,255.,255.]] #Josh
 	self.hsv_colors = ['black','orange','yellow','green','blue','violet','pink','all']
 
@@ -223,7 +223,7 @@ class Video():
 		self.blobs_box_pts.append(box_points)
 		cv2.drawContours(im2,[box_points],0,(255,255,255),2)
 	cv2.imwrite('contours.jpg',im2)
-	
+
 	if(colors=="any"):
 		pos_colors = np.zeros([8,3])	
 		pos_colors[0] = self.color_detection("black")
@@ -256,8 +256,7 @@ class Video():
 				return [np.zeros([1,3]),False]		
 		return [pos_colors,True] 
 	"""
-	self.color_detection("orange")
-	print "color finish"
+	self.color_detection("red")
 	"""
 
     def color_detection(self, color_str = 'blue'): #Josh
@@ -281,9 +280,9 @@ class Video():
 	if color_str == 'red':
 	    print "enter red"
 	    upper_bound1 = np.array([180.,255.,255.])
-	    lower_bound1 = np.array([170.,163.,80.])
+	    lower_bound1 = np.array([174.,100.,80.])
 	    upper_bound2 = np.array([3.,255.,255.])
-	    lower_bound2 = np.array([0.,163.,80.])
+	    lower_bound2 = np.array([0.,100.,80.])
 	    count = 0
 	    for box_points in self.blobs_box_pts_rgb_frame:
 		count += 1
@@ -297,7 +296,7 @@ class Video():
 		#cv2.circle(test_hsv,(centerx, centery), 2, (255,255,255), -1)
 	    #cv2.imwrite('test_center.jpg',test_hsv)
 		window = hsv_image[centerx-3:centerx+4, centery-3:centery+4]
-		#print window
+		print window
 		#restore = cv2.cvtColor(window, cv2.COLOR_HSV2BGR)
 		#cv2.imwrite('window_restore.jpg',restore)
 		mask1 = cv2.inRange(window, lower_bound1, upper_bound1)
@@ -405,9 +404,106 @@ class Video():
 	return [0.,0., 0.]
 
 	
+    def search_highest(self):
+	self.blobs_rectangles = []
+	self.blobs_box_pts = []
+        self.captureDepthFrame()
+	depth_image = self.currentDepthFrame
+	(dep_rows,dep_cols) = np.shape(depth_image)
+	min_row = 480
+	min_col = 640
+	max_row = 0
+	max_col = 100
+	min_dep_reading = 1000
+	max_height = 0
+	max_height_x = 0
+	max_height_y = 0
+	angle = 0
+ 	#get the rough range of board, 727 is the boundary of depth reading of board's surface and surfaces under it.
+	for cur_row in range(dep_rows-5):
+	    for cur_col in range(dep_cols-5):
+		if depth_image[cur_row][cur_col] < 727 and depth_image[cur_row+5][cur_col] < 727 and depth_image[cur_row][cur_col+5] < 727 and depth_image[cur_row+5][cur_col+5] < 727:
+		    if cur_row < min_row:
+			min_row = cur_row
+		    if cur_row > max_row:
+			max_row = cur_row
+                    if cur_col < min_col:
+			min_col = cur_col
+		    if cur_col>max_col:
+			max_col = cur_col
+	self.depth_image_min_row = min_row
+	self.depth_image_max_row = max_row
+	self.depth_image_min_col = min_col
+	self.depth_image_max_row = max_col
+	#print min_row, max_row
+	#print min_col, max_col
+	board_depth = depth_image[min_row:max_row,min_col:max_col]
+	#print np.shape(board_depth)
+	board_depth_binary = cv2.inRange(depth_image,500,715) # get binary image of depth frame by the reading of depth camera. 720 is the boundary of bricks surface and surfaces under them.
+	im2, contours,hierarchy = cv2.findContours(board_depth_binary,1,2)
+	for contour in contours:
+	    contour_area = cv2.contourArea(contour)
+	    # filter contours by area. Do experiment with Giri on Jan 23rd
+	    if contour_area>100 and contour_area<1000:
+		rectangle = cv2.minAreaRect(contour)
+		#print contour_area
+		#print rectangle
+		self.blobs_rectangles.append(rectangle)
+		box_points = cv2.boxPoints(rectangle)
+		box_points = np.int0(box_points)
+		self.blobs_box_pts.append(box_points)
+		cv2.drawContours(im2,[box_points],0,(255,255,255),2)
+	cv2.imwrite('contours.jpg',im2)
+	for box_points in self.blobs_box_pts:
+	    box_pts_rgb = []
+	    for point in box_points:
+		point = [point[0],point[1],1.]
+		rgb_point = np.dot(self.dep2rgb_aff_matrix,np.transpose(point))
+		box_pts_rgb.append([rgb_point[0],rgb_point[1]])
+	    box_pts_rgb = np.int0(box_pts_rgb)
+	    box_pts_rgb = np.array(box_pts_rgb)
+	    self.blobs_box_pts_rgb_frame.append(box_pts_rgb)
 
-
-
+	for box_points in self.blobs_box_pts_rgb_frame:
+	    pt1 = box_points[0]
+	    pt3 = box_points[2]
+	    centerx = np.int0((pt1[1] + pt3[1])/2) # box points (output from opencv function) has inverse order of xy compare to our x for row y for column
+	    centery = np.int0((pt1[0] + pt3[0])/2)
+	    #centerx = 280
+	    #centery = 403
+	    #self.blobs_center_rgb.append([centerx,centery])
+	    angle_yoverx = np.arctan2((pt1[1] - pt3[1]),(pt1[0] - pt3[0]))
+	    #cv2.circle(test_hsv,(centerx, centery), 2, (255,255,255), -1)
+	#cv2.imwrite('test_center.jpg',test_hsv)
+	    xy_in_rgb = np.float32([[[centery,centerx]]]) #Josh
+	    xy_in_rgb_homogenous = np.array([centery,centerx,1.0])
+	    xy_in_dep = np.dot(self.rgb2dep_aff_matrix, xy_in_rgb_homogenous) #Josh
+	    x_dep = int(xy_in_dep[0]) #Josh
+	    y_dep = int(xy_in_dep[1]) #Josh
+            d = self.currentDepthFrame[y_dep][x_dep]
+	    if (d>min_dep_reading):
+		continue
+	    min_dep_reading = d
+	    max_height_x = centerx
+	    max_height_y = centery
+	    angle = angle_yoverx
+	max_high_xy_in_rgb = np.float32([[[max_height_y,max_height_x]]])
+	Z = -1
+	for idx in range(len(self.levels_mm)):
+	    if min_dep_reading <=self.levels_upper_d[idx] and  min_dep_reading>=self.levels_lower_d[idx]:
+		Z = 941 - self.levels_mm[idx]
+		if Z<100:
+		    break
+	camera_coord = Z*cv2.undistortPoints(max_high_xy_in_rgb,self.intrinsic,self.distortion_array)
+	camera_coord_homogenous = np.transpose(np.append(camera_coord,[Z,1.0]))
+	world_coord_homogenous = np.dot(self.extrinsic, camera_coord_homogenous)
+	theta = np.arccos(self.extrinsic[0,0])
+	angle = angle - theta
+	#print  min_dep_reading
+	print world_coord_homogenous[0], world_coord_homogenous[1], world_coord_homogenous[2], angle
+	return world_coord_homogenous[0], world_coord_homogenous[1], world_coord_homogenous[2], angle
+		    
+		
 
 
 
